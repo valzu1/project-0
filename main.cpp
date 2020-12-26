4971f5ed68d7930f6e23c8ba459643c1e838582e
Valtteri Tarvainen <valtteri.tarvainen@gmail.com>
2020-12-26 16:22:21 +0200

    created the project

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <QApplication>
+#include "clickcounter.h"
+
+int main( int argc , char *argv[ ] ) {
+	QApplication app( argc , argv ) ;
+	Counter counter ;
+	counter.show( ) ;
+	return app.exec( ) ;
+}
+
